445942bba1d68082c3a0b4fc0025810c8bac018a
pedro092692 <pedro_092692@hotmail.com>
2024-10-22 18:10:04 -0400

    migrate and update database

--- app/models/product_keys.py
+++ app/models/product_keys.py
@@ -9,4 +9,4 @@ class ProductKeys(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     product_id: Mapped[int] = mapped_column(Integer, ForeignKey("products.id"))
     key_id: Mapped[int] = mapped_column(Integer, ForeignKey("keys.id"))
-    product_info: Mapped["products"] = relationship(back_populates="product_keys")
+    product_info: Mapped["products"] = relationship(back_populates="product_keys", cascade="all, delete-orphan")
